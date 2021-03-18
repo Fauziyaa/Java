62b180daf5a7f162760e9a1b916f2991077fb3a8
Fauziyaa <ansarifauziya183@gmail.com>
2021-03-18 11:59:05 +0530

    Initial Commit

--- src/com/company/Interfaces.java
+++ src/com/company/Interfaces.java
@@ -1,11 +1,16 @@
 package com.company;
 
 interface Bicycle{
+    int a = 40;
     void applyBreak(int increment);
     void speedUp(int decrement);
 }
 
-class BmwBicycle implements Bicycle{
+interface BicycleBody{
+    void color();
+    void tiers();
+}
+class BmwBicycle implements Bicycle, BicycleBody{
     @Override
     public void applyBreak(int decrement){
         System.out.println("Break is applied");
@@ -15,11 +20,28 @@ class BmwBicycle implements Bicycle{
     public void speedUp(int increment) {
         System.out.println("Speed is increased");
     }
+
+    @Override
+    public void color() {
+        System.out.println("red color");
+    }
+
+    @Override
+    public void tiers() {
+        System.out.println("CEAT tiers");
+    }
 }
+
 
 
 public class Interfaces {
     public static void main(String[] args) {
-
+    BmwBicycle bmw = new BmwBicycle();
+    bmw.applyBreak(3);
+    bmw.speedUp(2);
+    bmw.color();
+    bmw.tiers();
+        System.out.println(bmw.a); //we can only use attribute of interface
+//        bmw.a = 10;     //Can't change attributes of interface as it is final
     }
 }
